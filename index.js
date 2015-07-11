d22289f16d2b08a991100bb70b70d53f04147fda
Mu-An Chiou <me@muanchiou.com>
2015-07-11 12:55:44 -0700

    :speech_balloon: explained

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var mb = menubar({ dir: __dirname, height: 200, x: 0, y: 0 })
 mb.app.on('ready', function() {
   // Register a 'ctrl+shift+space' shortcut listener.
   var ret = globalShortcut.register('ctrl+shift+space', function() {
+    // It gets angry not knowing where to put the window if bounds not passed
     mb.tray.emit('clicked', null, {x: 0, y:0, width: 30, height: 30})
     console.log('ctrl+shift+space is pressed')
   })
