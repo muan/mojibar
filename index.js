01c0b36e3145c6ecc7709eed7afb2ca6144fa4be
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-22 12:37:28 -0700

    add option to launch mojibar at startup

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var menubar = require('menubar')
+var app = require('electron').app
 var ipc = require('electron').ipcMain
 var globalShortcut = require('electron').globalShortcut
-var mb = menubar({ dir: __dirname + '/app', width: 440, height: 230, icon: __dirname + '/app/Icon-Template.png', preloadWindow: true, windowPosition: 'topRight' })
+var mb = menubar({ dir: __dirname + '/app', width: 440, height: 270, icon: __dirname + '/app/Icon-Template.png', preloadWindow: true, windowPosition: 'topRight' })
 var Menu = require('electron').Menu
+var isDev = require('electron-is-dev')
 
 mb.on('show', function () {
   mb.window.webContents.send('show')
@@ -21,9 +23,17 @@ ipc.on('abort', function () {
   mb.hideWindow()
 })
 
-// when receive the abort message, close the app
+// update shortcuts when preferences change
 ipc.on('update-preference', function (evt, pref, initialization) {
   registerShortcut(pref['open-window-shortcut'], initialization)
+
+  // Make packaged app (not dev app) start at login
+  if (!isDev) {
+    app.setLoginItemSettings({
+      openAtLogin: pref['open-at-login'],
+      openAsHidden: true
+    })
+  }
 })
 
 var template = [
